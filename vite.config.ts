cbb6fc3093222db4d73ed99d11b18a200dd7d12c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-30 23:07:41 +0200

    chore: config path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    ViteAliases({
+      dir: 'src',
+      prefix: '@',
+      deep: true,
+      depth: 1,
+      createLog: false,
+      logPath: 'src/logs',
+      createGlobalAlias: true,
+      adjustDuplicates: false,
+      useAbsolute: false,
+      useIndexes: false,
+      useConfig: false,
+      useTypescript: true,
+      root: process.cwd(),
+    }),
+  ],
+});
