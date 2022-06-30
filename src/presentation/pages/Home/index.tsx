39126c76b6e55a2e90d528f89fa392d7b41dc25a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-30 23:17:36 +0200

    feat; create home component

---
+++ src/presentation/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+//import styles from './Home.module.css';
+
+export const Home: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
