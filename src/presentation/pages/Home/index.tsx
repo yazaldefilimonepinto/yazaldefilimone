471ce56133458125bae09b49cf27ca6819b2649d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-01 11:32:18 +0200

    feat: add project methopd

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Hero } from '@/presentation/components/Hero';
+import { Project } from '@/presentation/components/Project';
 import React, { FunctionComponent } from 'react';
 
 //import styles from './Home.module.css';
@@ -7,6 +8,7 @@ export const Home: FunctionComponent = () => {
   return (
     <article className="container h-5">
       <Hero />
+      <Project />
     </article>
   );
 };
