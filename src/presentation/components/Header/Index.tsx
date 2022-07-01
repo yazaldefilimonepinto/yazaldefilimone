27200b6521eae3cd65b9b28a213b45131e286b76
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-01 12:34:10 +0200

    feat: using navlink component

--- src/presentation/components/Header/Index.tsx
+++ src/presentation/components/Header/Index.tsx
@@ -1,43 +1,48 @@
 import React, { FunctionComponent } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from '../NavLink';
 
 import styles from './styles.module.scss';
 
 export const Header: FunctionComponent = () => {
   const [activeMenu, SetActiveMenu] = React.useState(false);
+  const headerLinks = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'About Me',
+      path: '/aboutMe',
+    },
+    {
+      name: 'Learning Journal',
+      path: '/learning-journal',
+    },
+    {
+      name: 'Blog',
+      path: '/blog',
+    },
+  ];
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <a href="/" className={styles.logo}>
+        <Link to="/" className={styles.logo}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
             <path fill="none" d="M0 0h24v24H0z" />
             <path d="M11 12l-7.071 7.071-1.414-1.414L8.172 12 2.515 6.343 3.929 4.93 11 12zm0 7h10v2H11v-2z" />
           </svg>
           Yazalde
-        </a>
+        </Link>
 
         <div className={styles.menu_container}>
           <ul className={activeMenu ? `${styles.menu_list} ${styles.showMenu}` : styles.menu_list}>
-            <li className={styles.menu_item}>
-              <a href="/" className={styles.menu_link}>
-                Home
-              </a>
-            </li>
-            <li className={styles.menu_item}>
-              <a href="/about" className={styles.menu_link}>
-                About me
-              </a>
-            </li>
-            <li className={styles.menu_item}>
-              <a href="/journal" className={styles.menu_link}>
-                Learning Journal
-              </a>
-            </li>
-            <li className={styles.menu_item}>
-              <a href="/blog" className={styles.menu_link}>
-                Blog
-              </a>
-            </li>
+            {headerLinks.map((link, key) => (
+              <li key={key} className={styles.menu_item}>
+                <NavLink path={link.path} name={link.name} />
+              </li>
+            ))}
           </ul>
           <button className={styles.hamburger} onClick={() => SetActiveMenu(!activeMenu)}>
             <span className={activeMenu ? styles.active : ''}></span>
