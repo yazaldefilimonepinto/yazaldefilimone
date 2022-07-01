7f5e3ca7da090b0172076a5e80d1688ea4ea777a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-01 09:59:13 +0200

    feat: create home hero

---
+++ src/presentation/components/Hero/index.tsx
@@ -0,0 +1,44 @@
+import React, { FunctionComponent } from 'react';
+
+import styles from './styles.module.scss';
+
+export const Hero: FunctionComponent = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.container}>
+        <span className={styles.happy}>Happy Friday!</span>
+        <span className={styles.hello}> </span>
+        <h1 className={styles.name}>I am Yazalde Filimone</h1>
+        <span className={styles.work}>
+          Software Developer at <span className={styles.style}>Reaktor</span>
+        </span>
+        <span className={styles.flow}>
+          Following the flow of{' '}
+          <span className={styles.style}>
+            learning creating teaching <i className="ri-rocket-2-fill"></i>
+          </span>
+        </span>
+      </div>
+
+      <div className={styles.content}>
+        <ul>
+          <li>
+            <a href="">
+              <i className="ri-github-line"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="ri-twitter-line"></i>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="ri-linkedin-box-line"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
