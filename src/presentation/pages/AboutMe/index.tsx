2c954504486c78b64ebe2477e30d62e35af9d7f1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-01 12:34:33 +0200

    feat: create about page

---
+++ src/presentation/pages/AboutMe/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+import styles from './styles.module.css';
+
+export const AboutMe: FunctionComponent = () => {
+  return (
+    <section className="container">
+      <h2>AboutMe</h2>
+    </section>
+  );
+};
