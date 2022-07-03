fe53b11dbbfb60c8187fe198aac12d06ae7b951e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 16:00:52 +0200

    feat: add colors components

--- src/presentation/components/Setting/index.tsx
+++ src/presentation/components/Setting/index.tsx
@@ -1,12 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { Colors } from '@/presentation/components/Colors';
 
 import styles from './styles.module.scss';
 
 export const Setting: FunctionComponent = () => {
   const [active, SetActive] = React.useState(false);
+  const [color, SetColor] = React.useState(false);
 
   function handlerActive() {
     SetActive(!active);
+    SetColor(false);
+  }
+  function handlerColor() {
+    SetColor(!color);
   }
 
   return (
@@ -16,17 +22,21 @@ export const Setting: FunctionComponent = () => {
           <i className="ri-settings-2-line"></i>
         </button>
         <ul className={active ? `${styles.content_list} ${styles.active}` : styles.content_list}>
-          <li>
+          <li className={styles.action_container}>
             <button className={styles.action}>
               <i className="ri-admin-fill"></i>
             </button>
           </li>
-          <li>
-            <button className={styles.action}>
-              <i className="ri-shield-fill"></i>
+          <li className={styles.action_container}>
+            <button
+              className={color ? `${styles.action} ${styles.actionColor}` : styles.action}
+              onClick={handlerColor}
+            >
+              <i className={color ? `ri-shield-fill ${styles.actionColor}` : 'ri-shield-fill'}></i>
             </button>
+            <Colors active={color} SetActive={SetColor} />
           </li>
-          <li>
+          <li className={styles.action_container}>
             <button className={styles.action}>
               <i className="ri-mail-send-fill"></i>
             </button>
