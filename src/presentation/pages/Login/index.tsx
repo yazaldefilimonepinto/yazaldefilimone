0a15a26d174754433151b1880adbeb3b58bbaaac
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 15:10:31 +0200

    feat:create login page

---
+++ src/presentation/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+//import styles from './styles.module.css';
+
+export const Login: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+    </div>
+  );
+};
