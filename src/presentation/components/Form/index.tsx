8caacda8ae44dd86436cb0d016d59f3bef7bfeb2
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 19:26:11 +0200

    feat: create form component

--- src/presentation/components/Form/index.tsx
+++ src/presentation/components/Form/index.tsx
@@ -4,16 +4,19 @@ import styles from './styles.module.scss';
 
 export const Form: FunctionComponent = () => {
   return (
-    <form className={styles.container}>
-      <div className={styles.input}>
-        <input type="email" placeholder="Enter your email" />
-        <i className="ri-user-6-line"></i>
+    <form className={styles.content}>
+      <h2>Login Adm</h2>
+      <div className={styles.container}>
+        <div className={styles.input}>
+          <input type="email" placeholder="Enter your email" />
+          <i className="ri-mail-line"></i>
+        </div>
+        <div className={styles.input}>
+          <input type="password" placeholder="Enter your password" />
+          <i className="ri-lock-line"></i>
+        </div>
+        <button className={styles.button}>Login</button>
       </div>
-      <div className={styles.input}>
-        <input type="password" placeholder="Enter your password" />
-        <i className="ri-lock-line"></i>
-      </div>
-      <button className={styles.button}>Login</button>
     </form>
   );
 };
