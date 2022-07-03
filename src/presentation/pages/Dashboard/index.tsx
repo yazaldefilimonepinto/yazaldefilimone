b24cd92a697089c35403a094e24cabc1e44ff0d6
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-03 21:16:49 +0200

    feat: create dashboard

---
+++ src/presentation/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+import styles from './styles.module.css';
+
+export const Dashboard: FunctionComponent = () => {
+  return <section></section>;
+};
